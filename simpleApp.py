0f2ebfb591512138d4787d38e2ae0cb7a17f2c68
phurinat-udo <phurinat.udo@outlook.com>
2025-04-24 15:10:47 +0700

    upgrade code

--- simpleApp.py
+++ simpleApp.py
@@ -1,12 +1,23 @@
 import dash
-from dash import html
+from dash import html, Output, Input
 
 # Create the app
 app = dash.Dash(__name__)
 
 # Define layout
-app.layout = html.Div("Hello, Mars!")
+app.layout = html.Div([
+    html.Button("Click me", id="my-button", n_clicks=0),
+    html.Div(id="output-div")
+])
+
+# Callback to update the text based on number of clicks
+@app.callback(
+    Output("output-div", "children"),
+    Input("my-button", "n_clicks")
+)
+def update_output(n_clicks):
+    return f"You clicked the button {n_clicks} times."
 
 # Run the app
 if __name__ == "__main__":
-    app.run(debug=True, port=36646)
+    app.run(debug=True, port=5001)
